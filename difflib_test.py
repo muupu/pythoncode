0ec74dfa889ff870d18a9562af5638e0681abb92
muupu <once645@gmail.com>
2016-01-17 22:33:37 +0800

    if no blocks match

--- difflib_test.py
+++ difflib_test.py
@@ -12,3 +12,9 @@ print lm
 s = difflib.SequenceMatcher(lambda x: x==" ", " abcd", "abcd abcd")
 lm = s.find_longest_match(0, 5, 0, 9)
 print lm
+# Match(a=1, b=0, size=4)
+
+### no blocks match ###
+s = difflib.SequenceMatcher(None, "ab", "c")
+lm = s.find_longest_match(0, 2, 0, 1)
+print lm
