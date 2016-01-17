f344c278a6fbbe269deed7f30189d880fbca02ad
muupu <once645@gmail.com>
2016-01-17 22:27:16 +0800

    print

--- difflib_test.py
+++ difflib_test.py
@@ -4,4 +4,5 @@ import difflib
 
 ### find_longest_match ###
 s = difflib.SequenceMatcher(None, " abcd", "abcd abcd")
-s.find_longest_match(0, 5, 0, 9)
+lm = s.find_longest_match(0, 5, 0, 9)
+print lm
