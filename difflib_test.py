bc0a9239fcb7614d2cc3682eaab126ca68f21f75
muupu <once645@gmail.com>
2016-01-17 22:30:22 +0800

    isjunk is defined

--- difflib_test.py
+++ difflib_test.py
@@ -6,3 +6,9 @@ import difflib
 s = difflib.SequenceMatcher(None, " abcd", "abcd abcd")
 lm = s.find_longest_match(0, 5, 0, 9)
 print lm
+# Out[3]: Match(a=0, b=4, size=5)
+
+### isjunk is defined ###
+s = SequenceMatcher(lambda x: x==" ", " abcd", "abcd abcd")
+lm = s.find_longest_match(0, 5, 0, 9)
+print lm
