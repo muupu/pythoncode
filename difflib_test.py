7ef57e0677c343eecee50f5a9d767bb4c67c2fc8
qiaoda.zqd <qiaoda.zqd@alibaba-inc.com>
2016-01-20 11:10:21 +0800

    add main

--- difflib_test.py
+++ difflib_test.py
@@ -2,32 +2,38 @@
 
 from difflib_comments import SequenceMatcher
 
-### find_longest_match ###
-s = SequenceMatcher(None, " abcd", "abcd abcd")
-lm = s.find_longest_match(0, 5, 0, 9)
-print lm
-# Out[3]: Match(a=0, b=4, size=5)
+def test_find_longest_match():
+	### find_longest_match ###
+	s = SequenceMatcher(None, " abcd", "abcd abcd")
+	lm = s.find_longest_match(0, 5, 0, 9)
+	print lm
+	# Out[3]: Match(a=0, b=4, size=5)
 
-### isjunk is defined ###
-s = SequenceMatcher(lambda x: x==" ", " abcd", "abcd abcd")
-lm = s.find_longest_match(0, 5, 0, 9)
-print lm
-# Match(a=1, b=0, size=4)
+	### isjunk is defined ###
+	s = SequenceMatcher(lambda x: x==" ", " abcd", "abcd abcd")
+	lm = s.find_longest_match(0, 5, 0, 9)
+	print lm
+	# Match(a=1, b=0, size=4)
 
-### no blocks match ###
-s = SequenceMatcher(None, "ab", "c")
-lm = s.find_longest_match(0, 2, 0, 1)
-print lm
-# Match(a=0, b=0, size=0)
+	### no blocks match ###
+	s = SequenceMatcher(None, "ab", "c")
+	lm = s.find_longest_match(0, 2, 0, 1)
+	print lm
+	# Match(a=0, b=0, size=0)
 
-### stripping common prefix or suffix ###
-s = SequenceMatcher(None, "ab", "acab")
-lm = s.find_longest_match(0, 2, 0, 4)
-print lm
-# Match(a=0, b=0, size=1)
+	### stripping common prefix or suffix ###
+	s = SequenceMatcher(None, "ab", "acab")
+	lm = s.find_longest_match(0, 2, 0, 4)
+	print lm
+	# Match(a=0, b=0, size=1)
 
-### find_longest_match ###
-s = SequenceMatcher(None, "abcdeabce", "abcecabd")
-lm = s.find_longest_match(0, 9, 0, 8)
-print lm
-# Out[3]: Match(a=5, b=0, size=4)
+def test_find_longest_match2():
+	### find_longest_match ###
+	s = SequenceMatcher(None, "abcdeabce", "abcecabd")
+	lm = s.find_longest_match(0, 9, 0, 8)
+	print lm
+	# Out[3]: Match(a=5, b=0, size=4)
+
+if __name__ == "__main__":
+    # test_find_longest_match()
+    test_find_longest_match2()
