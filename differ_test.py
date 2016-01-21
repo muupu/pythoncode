9d30310165a8d16ebe9a490b36e038e99c1a9cfb
muupu <once645@gmail.com>
2016-01-21 22:07:55 +0800

    test

--- differ_test.py
+++ differ_test.py
@@ -4,18 +4,18 @@ from difflib_comments import SequenceMatcher, Differ
 
 def test_Differ():
 	text1 = '''  1. Beautiful is better than ugly.
-			2. Explicit is better than implicit.
-			3. Simple is better than complex.
-			4. Complex is better than complicated.
-			'''.splitlines(1)
-	print len(text1)
+2. Explicit is better than implicit.
+3. Simple is better than complex.
+4. Complex is better than complicated.
+'''.splitlines(1)
+	print text1
 	print text1[0][-1]
 	text2 = '''  1. Beautiful is better than ugly.
-			3.   Simple is better than complex.
-			4. Complicated is better than complex.
-			5. Flat is better than nested.
-			'''.splitlines(1)
-	print len(text2)
+3.   Simple is better than complex.
+4. Complicated is better than complex.
+5. Flat is better than nested.
+'''.splitlines(1)
+	print text2
 	print text2[0][-1]
 
 
