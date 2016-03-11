d552fe75247a4e94a0e8be7169cd002a73028416
muupu <once645@gmail.com>
2016-03-11 09:44:02 +0800

    fix

--- tcp_server_test.py
+++ tcp_server_test.py
@@ -10,7 +10,7 @@ def tcplink(sock, addr):
     sock.send('Welcome!')
     while True:
         data = sock.recv(1024)
-        print "receive data: %s" % data
+        print "receive data:" + data
         time.sleep(1)
         if data == 'exit' or not data:
             break
