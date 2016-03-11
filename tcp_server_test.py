3b32bfe4dc6b27013962524abe21f5d7a704ac10
muupu <once645@gmail.com>
2016-03-11 09:46:44 +0800

    print data len

--- tcp_server_test.py
+++ tcp_server_test.py
@@ -10,7 +10,7 @@ def tcplink(sock, addr):
     sock.send('Welcome!')
     while True:
         data = sock.recv(1024)
-        print "receive data:" + data
+        print "receive data:" + data + " len:" + len(data)
         time.sleep(1)
         if data == 'exit' or not data:
             break
