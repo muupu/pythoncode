fb90dc0c2b32aae5a764b5eba6a6df637db8b7a5
muupu <once645@gmail.com>
2016-05-07 21:54:13 +0800

    add asyncioexample.py

---
+++ AsyncioExample.py
@@ -0,0 +1,19 @@
+import asyncio
+
+@asyncio.coroutine
+def work():
+
+@asyncio.coroutine
+def hello():
+	workers = [asyncio.Task(work(), loop=self.loop)
+                   for _ in range(5)]
+    print("Hello world!")
+    # 异步调用asyncio.sleep(1):
+    r = yield from asyncio.sleep(1)
+    print("Hello again!")
+
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+# 执行coroutine
+loop.run_until_complete(hello())
+loop.close()
